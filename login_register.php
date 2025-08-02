b80d64285ee61dc71343b9ae4697ef268b5e37c8
jacksoncity <jacksonstokke@gmail.com>
2025-08-02 13:29:13 -0400

    inital commit

---
+++ login_register.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+require_once 'config.php';
+
+if (isset($_POST[''])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE user = '$user'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['username'] = $user['username'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin-page.php");
+            } else {
+                header("Location: main.html");
+            }
+            exit();
+        }
+    }
+
+    $_SESSION['login_error'] = 'Incorrect username or password';
+    header("Location: adminlogin.php");
+    exit();
+}
